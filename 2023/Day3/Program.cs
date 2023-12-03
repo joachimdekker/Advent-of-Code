95de803a7d150c619b2b20962725e54d6cdcc78c
Joachim Dekker <jdekker3008@gmail.com>
2023-12-03 12:25:47 +0100

    Start with day3 - part 2

--- 2023/Day3/Program.cs
+++ 2023/Day3/Program.cs
@@ -11,34 +11,33 @@ for (int i = 0; i < engine.Length; i++)
 {
     for (int j = 0; j < engine[i].Length; j++)
     {
-        if (char.IsDigit(engine[i][j]))
+        if (engine[i][j] == '*')
         {
-            digits.Add(engine[i][j]);
+            int[] nums = LookAroundForNums(i, j);
 
-            if (LookaroundForParts(i, j))
+            if(nums.Length == 2)
             {
-                foundPartNumber = true;
+                sum += nums[0] * nums[1];
             }
-        }
-        else
-        {
-            if (foundPartNumber)
-            {
-                Console.WriteLine($"Found part number: {string.Join("", digits)} ({i},{j})");
-                sum += int.Parse(string.Join("", digits));
-            }
-            else
-            {
-                if (digits.Count > 0)
-                {
-                    Console.WriteLine($"Found no part number for digits: {string.Join("", digits)} ({i},{j})");
-                }
-            }
-
-            foundPartNumber = false;
-            digits.Clear();
         }
     }
+/*
+    if (foundPartNumber)
+    {
+        Console.WriteLine($"Found part number: {string.Join("", digits)} (EOL)");
+        sum += int.Parse(string.Join("", digits));
+    }
+    else
+    {
+        if (digits.Count > 0)
+        {
+            Console.WriteLine($"Found no part number for digits: {string.Join("", digits)} (EOL)");
+        }
+    }
+
+    foundPartNumber = false;
+    digits.Clear();*/
+
 }
 
 Console.WriteLine($"Sum: {sum}");
@@ -52,6 +51,7 @@ bool LookaroundForParts(int i, int j)
     {
         for (int y = ymin; y <= ymax; y++)
         {
+
             char[] line = engine[x];
             char char1 = line[y];
             if (parts.Contains(char1))
@@ -62,4 +62,61 @@ bool LookaroundForParts(int i, int j)
     }
 
     return false;
+}
+
+
+int[] LookAroundForNums(int i, int j)
+{
+    (int xmin, int xmax) = (Math.Max(0, i - 1), Math.Min(engine.Length - 1, i + 1));
+    (int ymin, int ymax) = (Math.Max(0, j - 1), Math.Min(engine[i].Length - 1, j + 1));
+
+    List<int> nums = [];
+
+    for (int x = xmin; x <= xmax; x++)
+    {
+        for (int y = ymin; y <= ymax; y++)
+        {
+
+            char[] line = engine[x];
+            char char1 = line[y];
+            if (char.IsDigit(engine[x][y]))
+            {
+                int num = GetNum(x, y);
+                if(!nums.Contains(num))
+                {
+                    nums.Add(GetNum(x, y));
+                }
+            }
+        }
+    }
+
+    return nums.ToArray();
+}
+
+int GetNum(int i, int j)
+{
+    int num = 0;
+    int multiplier = 1;
+
+    int jTemp = j;
+
+    List<char> digits = [engine[i][j]];
+
+    jTemp--;
+
+    while (char.IsDigit(engine[i][j]) && jTemp > 0)
+    {
+        digits.Insert(0, engine[i][jTemp]);
+        jTemp--;
+    }
+
+    jTemp = j;
+
+    while (char.IsDigit(engine[i][j]) && jTemp < engine[i].Length - 1)
+    {
+        digits.Add(engine[i][jTemp]);
+        jTemp++;
+    }
+
+    return num;
 }
