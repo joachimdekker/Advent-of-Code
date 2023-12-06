41535305111c2022b98dda95925e0b00ba014dfd
Joachim Dekker <jdekker3008@gmail.com>
2023-12-06 22:19:03 +0100

    Add day4

---
+++ 2023/Day4/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+int sum = 0;
+
+int[] totalCards = new int[lines.Length];
+for (int i = 0; i < lines.Length; i++)
+{
+    totalCards[i] = 1;
+}
+
+foreach (string line in lines)
+{
+    string[] split = line.Split('|');
+    string[] card = split[0].Split(":");
+
+    int cardNum = int.Parse(card[0].Split(" ")[^1]);
+    int[] winningNumbers = card[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    int[] ownNumbers = split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    int prices = ownNumbers.Where(winningNumbers.Contains).Count();
+
+    for (int i = cardNum; i < cardNum + prices && i < lines.Length; i++)
+    {
+        totalCards[i] += totalCards[cardNum - 1];
+    }
+}
+
+sum = totalCards.Sum();
+
+Console.WriteLine(sum);
